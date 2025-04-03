4d03d9de82af53bea74e71fbf9b9484a043fff1f
endozzgz <quique17zgz@gmail.com>
2025-04-03 15:12:39 +0200

    flows search

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -8,12 +8,36 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('Accedo a la PDP con el search', async ({ page }) => {
+  await page.goto('https://www.loewe.com/eur/en/home');
+  await page.waitForTimeout(1000); // Pause for 1 second
+  await page.locator('.css-w48aea.edafrmd4').click();
+  await page.waitForTimeout(1000); // Pause for 1 second
+  await page.getByPlaceholder('Buscar').fill('bolso');
+  await page.waitForTimeout(8000); // Pause for 1 second
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  const cookiesPopup = page.locator('#onetrust-accept-btn-handler'); // Updated selector for the accept button
+  if (await cookiesPopup.isVisible()) {
+    await cookiesPopup.click();
+    await page.waitForTimeout(3000); // Pause for 1 second after accepting cookies
+  }
+  await page.getByText('Bolso Puzzle pequeño en piel de ternera Burdeos Oscuro').click();
+  await page.waitForTimeout(2000); // Pause for 2 seconds
+  await expect(page).toHaveURL('https://www.loewe.com/eur/es/mujer/bolsos/puzzle/bolso-puzzle-pequeno-en-piel-de-ternera/A510S21XAE-7240.html');
+});
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test('Accedo a la PLP con el search', async ({ page }) => {
+  await page.goto('https://www.loewe.com/eur/en/home', { waitUntil: 'load' });
+  await page.locator('.css-w48aea.edafrmd4').click();
+  await page.waitForLoadState('load'); 
+  await page.getByPlaceholder('Buscar').fill('bolso');
+ 
+
+  const cookiesPopup = page.locator('#onetrust-accept-btn-handler'); 
+  await cookiesPopup.waitFor({ state: 'visible', timeout: 8000 }); 
+  if (await cookiesPopup.isVisible()) {
+    await cookiesPopup.click();
+  }
+  await page.getByTestId('header-search-submit-button').click();
+  await page.waitForURL('https://www.loewe.com/eur/es/search?q=bolso', { waitUntil: 'load' });
 });
